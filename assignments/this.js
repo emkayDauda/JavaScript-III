cb11af3dbd68a566b0f70aea76bc1ebd9bccd875
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-21 12:07:08 +0100

    Answer this.js questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Object Binding: 'this' here refers to the window or the global object. Basically, the outer level (scope) of the file?
+*
+* 2. Implicit Binding: Here, 'this' is implicitly understood to mean the scope of the current object. It is not specified or assigned directly.
+*
+* 3. Explicit Binding: 'this', with explicit binding, is outrightly assigned or modified and it begins to refer to the new assigned value.
+*
+* 4. New Binding: Constructors are blueprints used to create objects. Whenever a constructor is used to create an object, 'this' refers to the specific instance of the object that was just created. This meins when more than one object is created off of the same constructor, each object has a `this` specific to it.
 *
 * write out a code example of each explanation above
 */
